d20db47f6f17770b9d8c217a2f937a00e13ea69d
Shivam Kumar <csautomaytic@gmail.com>
2024-11-19 15:48:27 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,6 +40,7 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       white: '#fff',
+      arrow: 'rgb(178, 149, 201)',
       'modal-backdrop-light': 'rgba(0, 0, 0, 0.4)',
       'modal-backdrop-dark': 'rgba(0, 0, 0, 0.6)',
       current: 'currentColor',
