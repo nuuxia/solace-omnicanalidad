68160ef9f4567de1416cb92864e6b1f88f7006c7
Tomas Minissale <minissaletomas@gmail.com>
2024-12-03 12:07:55 -0300

    setting staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -13,7 +13,7 @@ set :branch, 'chatwoot_new_version'
 # Configuración de Puma (puedes dejarla igual que en el archivo `deploy.rb`)
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :puma_bind, 'tcp://127.0.0.1:3000'
+set :puma_bind, 'tcp://127.0.0.1:4001'
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
