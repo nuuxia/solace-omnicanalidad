a62d55dea8272782352c41a8ac83c95f4b4aa5e2
Tomas Minissale <minissaletomas@gmail.com>
2024-12-04 14:51:26 -0300

    fixing code

--- app/jobs/send_reply_job.rb
+++ app/jobs/send_reply_job.rb
@@ -10,7 +10,7 @@ class SendReplyJob < ApplicationJob
       'Channel::TwitterProfile' => ::Twitter::SendOnTwitterService,
       'Channel::TwilioSms' => ::Twilio::SendOnTwilioService,
       'Channel::Line' => ::Line::SendOnLineService,
-      'Channel::MercadoLibre' => ::Line::SendOnMercadoLibreService,
+      'Channel::MercadoLibre' => ::MercadoLibre::SendOnMercadoLibreService,
       'Channel::Telegram' => ::Telegram::SendOnTelegramService,
       'Channel::Whatsapp' => ::Whatsapp::SendOnWhatsappService,
       'Channel::Sms' => ::Sms::SendOnSmsService
