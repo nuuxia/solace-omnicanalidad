274aa3686147dbced199e597fdb1fd80c459bafa
Darwin Alves <darwin-pc@192.168.1.25>
2025-02-14 20:10:52 -0300

    test

--- app/services/whatsapp/providers/whatsapp_cloud_service.rb
+++ app/services/whatsapp/providers/whatsapp_cloud_service.rb
@@ -1,150 +1,162 @@
-module Whatsapp
-  module Providers
-    class AutomatedWhatsappCloudService
-      def initialize(account_id:, waba_id:, phone_number_id:)
-        @account_id = account_id
-        @waba_id = waba_id
-        @phone_number_id = phone_number_id
-        @version = ENV['VITE_FB_GRAPH_API_VERSION']
-        @access_token = ENV['SYSTEM_USER_ACCESS_TOKEN']
-        @pin = ENV['VITE_FB_CONFIG_PIN']
-      end
-
-      def call
-        Rails.logger.info "in service: #{@waba_id} y phone_number_id: #{@phone_number_id}"
-
-        unless valid_environment_variables?
-          Rails.logger.error "❌ Error: Necessary environment variables are missing"
-          return nil
-        end
-
-        if register_phone_number && subscribe_app_to_waba
-          details = fetch_and_log_phone_number_details
-          inbox = create_inbox(details) if details
-          return {
-            inbox_id: inbox&.id,
-            waba_id: @waba_id,
-            phone_number_id: @phone_number_id
-          }
-        end
-
-        nil
-      end
-
-      private
-
-      def valid_environment_variables?
-        @version.present? && @access_token.present? && @pin.present?
-      end
-
-      def register_phone_number
-        url = "https://graph.facebook.com/#{@version}/#{@phone_number_id}/register"
-
-        response = Faraday.post(url) do |req|
-          req.headers['Authorization'] = "Bearer #{@access_token}"
-          req.headers['Content-Type'] = 'application/json'
-          req.body = {
-            messaging_product: 'whatsapp',
-            pin: @pin
-          }.to_json
-        end
-
-        handle_response(response, "Registro del número de teléfono")
-      end
-
-      def subscribe_app_to_waba
-        url = "https://graph.facebook.com/#{@version}/#{@waba_id}/subscribed_apps"
-
-        response = Faraday.post(url) do |req|
-          req.headers['Authorization'] = "Bearer #{@access_token}"
-          req.headers['Content-Type'] = 'application/json'
-          req.body = {}.to_json
-        end
-
-        handle_response(response, "Suscripción de la aplicación al WABA")
-      end
-
-      def fetch_and_log_phone_number_details
-        url = "https://graph.facebook.com/#{@version}/#{@waba_id}/phone_numbers"
-
-        response = Faraday.get(url) do |req|
-          req.headers['Authorization'] = "Bearer #{@access_token}"
-        end
-
-        if response.success?
-          process_phone_number_response(response.body)
-        else
-          Rails.logger.error "⛔️ Error in Request for phone number details. Code: #{response.status}, Response: #{response.body}"
-          nil
-        end
-      rescue StandardError => e
-        Rails.logger.error "⚠️ Exception during Request for telephone number details: #{e.message}"
-        nil
-      end
-
-      def process_phone_number_response(response_body)
-        data = JSON.parse(response_body)['data']
-
-        matching_phone_number = data.find { |phone| phone['id'] == @phone_number_id }
-
-        if matching_phone_number
-          display_phone_number = normalize_phone_number(matching_phone_number['display_phone_number'])
-          verified_name = matching_phone_number['verified_name']
-
-          {
-            inbox_name: verified_name,
-            phone_number: display_phone_number,
-            phone_number_id: @phone_number_id,
-            business_account_id: @waba_id,
-            api_key: @access_token
-          }
-        else
-          Rails.logger.warn "⚠️ No matching phone number found for phone_number_id #{@phone_number_id}"
-          nil
-        end
-      end
-
-      def normalize_phone_number(phone_number)
-        "+" + phone_number.gsub(/[^\d]/, '')
-      end
-
-      def create_inbox(details)
-        begin
-          channel = Channel::Whatsapp.create!(
-            account_id: @account_id,
-            phone_number: details[:phone_number],
-            provider: 'whatsapp_cloud',
-            provider_config: {
-              api_key: details[:api_key],
-              phone_number_id: details[:phone_number_id],
-              business_account_id: details[:business_account_id],
-            }
-          )
-
-          inbox = Inbox.create!(
-            account_id: @account_id,
-            name: details[:inbox_name],
-            channel: channel
-          )
-
-          inbox
-        rescue StandardError => e
-          Rails.logger.error "⛔️ Error creating the inbox: #{e.message}"
-          raise e
-        end
-      end
-
-      def handle_response(response, action_name)
-        if response.success?
-          true
-        else
-          Rails.logger.error "⛔️ Error in #{action_name}. Code: #{response.status}, Response: #{response.body}"
-          false
-        end
-      rescue StandardError => e
-        Rails.logger.error "⚠️ Exception in #{action_name}: #{e.message}"
-        false
-      end
+class Whatsapp::Providers::WhatsappCloudService < Whatsapp::Providers::BaseService
+  def send_message(phone_number, message)
+    if message.attachments.present?
+      send_attachment_message(phone_number, message)
+    elsif message.content_type == 'input_select'
+      send_interactive_text_message(phone_number, message)
+    else
+      send_text_message(phone_number, message)
     end
+  end
+
+  def send_template(phone_number, template_info)
+    response = HTTParty.post(
+      "#{phone_id_path}/messages",
+      headers: api_headers,
+      body: {
+        messaging_product: 'whatsapp',
+        to: phone_number,
+        template: template_body_parameters(template_info),
+        type: 'template'
+      }.to_json
+    )
+
+    process_response(response)
+  end
+
+  def sync_templates
+    # ensuring that channels with wrong provider config wouldn't keep trying to sync templates
+    whatsapp_channel.mark_message_templates_updated
+    templates = fetch_whatsapp_templates("#{business_account_path}/message_templates?access_token=#{whatsapp_channel.provider_config['api_key']}")
+    whatsapp_channel.update(message_templates: templates, message_templates_last_updated: Time.now.utc) if templates.present?
+  end
+
+  def fetch_whatsapp_templates(url)
+    response = HTTParty.get(url)
+    return [] unless response.success?
+
+    next_url = next_url(response)
+
+    return response['data'] + fetch_whatsapp_templates(next_url) if next_url.present?
+
+    response['data']
+  end
+
+  def next_url(response)
+    response['paging'] ? response['paging']['next'] : ''
+  end
+
+  def validate_provider_config?
+    response = HTTParty.get("#{business_account_path}/message_templates?access_token=#{whatsapp_channel.provider_config['api_key']}")
+    response.success?
+  end
+
+  def api_headers
+    { 'Authorization' => "Bearer #{whatsapp_channel.provider_config['api_key']}", 'Content-Type' => 'application/json' }
+  end
+
+  def media_url(media_id)
+    "#{api_base_path}/v13.0/#{media_id}"
+  end
+
+  def api_base_path
+    ENV.fetch('WHATSAPP_CLOUD_BASE_URL', 'https://graph.facebook.com')
+  end
+
+  # TODO: See if we can unify the API versions and for both paths and make it consistent with out facebook app API versions
+  def phone_id_path
+    "#{api_base_path}/v13.0/#{whatsapp_channel.provider_config['phone_number_id']}"
+  end
+
+  def business_account_path
+    "#{api_base_path}/v14.0/#{whatsapp_channel.provider_config['business_account_id']}"
+  end
+
+  def send_text_message(phone_number, message)
+    response = HTTParty.post(
+      "#{phone_id_path}/messages",
+      headers: api_headers,
+      body: {
+        messaging_product: 'whatsapp',
+        context: whatsapp_reply_context(message),
+        to: phone_number,
+        text: { body: message.content },
+        type: 'text'
+      }.to_json
+    )
+
+    process_response(response)
+  end
+
+  def send_attachment_message(phone_number, message)
+    attachment = message.attachments.first
+    type = %w[image audio video].include?(attachment.file_type) ? attachment.file_type : 'document'
+    type_content = {
+      'link': attachment.download_url
+    }
+    type_content['caption'] = message.content unless %w[audio sticker].include?(type)
+    type_content['filename'] = attachment.file.filename if type == 'document'
+    response = HTTParty.post(
+      "#{phone_id_path}/messages",
+      headers: api_headers,
+      body: {
+        :messaging_product => 'whatsapp',
+        :context => whatsapp_reply_context(message),
+        'to' => phone_number,
+        'type' => type,
+        type.to_s => type_content
+      }.to_json
+    )
+
+    process_response(response)
+  end
+
+  def process_response(response)
+    if response.success?
+      response['messages'].first['id']
+    else
+      Rails.logger.error response.body
+      nil
+    end
+  end
+
+  def template_body_parameters(template_info)
+    {
+      name: template_info[:name],
+      language: {
+        policy: 'deterministic',
+        code: template_info[:lang_code]
+      },
+      components: [{
+        type: 'body',
+        parameters: template_info[:parameters]
+      }]
+    }
+  end
+
+  def whatsapp_reply_context(message)
+    reply_to = message.content_attributes[:in_reply_to_external_id]
+    return nil if reply_to.blank?
+
+    {
+      message_id: reply_to
+    }
+  end
+
+  def send_interactive_text_message(phone_number, message)
+    payload = create_payload_based_on_items(message)
+
+    response = HTTParty.post(
+      "#{phone_id_path}/messages",
+      headers: api_headers,
+      body: {
+        messaging_product: 'whatsapp',
+        to: phone_number,
+        interactive: payload,
+        type: 'interactive'
+      }.to_json
+    )
+
+    process_response(response)
   end
 end
