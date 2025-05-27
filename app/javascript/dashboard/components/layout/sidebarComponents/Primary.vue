2c22c3612d0c999901879cc9613f42e013a204be
Darwin Alves <darwinalveswork@gmail.com>
2025-05-27 16:19:36 -0300

    Prod Fixes

--- app/javascript/dashboard/components/layout/sidebarComponents/Primary.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/Primary.vue
@@ -18,10 +18,10 @@ export default {
     NotificationBell,
   },
   props: {
-    isACustomBrandedInstance: {
+    /* isACustomBrandedInstance: {
       type: Boolean,
       default: false,
-    },
+    }, */
     logoSource: {
       type: String,
       default: '',
@@ -91,7 +91,7 @@ export default {
       />
     </div>
     <div class="flex flex-col items-center justify-end pb-6">
-      <a
+      <!--  <a
         v-if="!isACustomBrandedInstance"
         v-tooltip.right="$t(`SIDEBAR.DOCS`)"
         :href="helpDocsURL"
@@ -101,7 +101,7 @@ export default {
       >
         <fluent-icon icon="book-open-globe" />
         <span class="sr-only">{{ $t(`SIDEBAR.DOCS`) }}</span>
-      </a>
+      </a> -->
       <NotificationBell @open-notification-panel="openNotificationPanel" />
       <AgentDetails @toggle-menu="toggleOptions" />
       <OptionsMenu
